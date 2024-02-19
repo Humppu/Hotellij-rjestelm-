6d4182c5c6ffa53a74cfe19e7be4f152359ad32d
Humppu <142491124+Humppu@users.noreply.github.com>
2024-02-19 09:19:36 +0200

    Add files via upload
    Valmis

--- Kirjautumisikkuna/Kirjautumisikkuna/Kirjautumisikkuna.Designer.cs
+++ Kirjautumisikkuna/Kirjautumisikkuna/Kirjautumisikkuna.Designer.cs
@@ -29,15 +29,17 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.KirjauduLB = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SalasanaTB = new System.Windows.Forms.TextBox();
-            this.KayttajatunnusTB = new System.Windows.Forms.TextBox();
-            this.KirjauduBT = new System.Windows.Forms.Button();
+            this.KayttajaTB = new System.Windows.Forms.TextBox();
+            this.KirjauduPainike = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -50,22 +52,22 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // KirjauduLB
             // 
-            this.label1.BackColor = System.Drawing.Color.Yellow;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 165);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(497, 56);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Kirjaudu sisään";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.KirjauduLB.BackColor = System.Drawing.Color.Yellow;
+            this.KirjauduLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KirjauduLB.Location = new System.Drawing.Point(115, 10);
+            this.KirjauduLB.Name = "KirjauduLB";
+            this.KirjauduLB.Size = new System.Drawing.Size(275, 41);
+            this.KirjauduLB.TabIndex = 1;
+            this.KirjauduLB.Text = "Kirjaudu sisään";
+            this.KirjauduLB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.SalasanaTB);
-            this.groupBox1.Controls.Add(this.KayttajatunnusTB);
-            this.groupBox1.Controls.Add(this.KirjauduBT);
+            this.groupBox1.Controls.Add(this.KayttajaTB);
+            this.groupBox1.Controls.Add(this.KirjauduPainike);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(12, 224);
@@ -83,26 +85,28 @@
             this.SalasanaTB.Size = new System.Drawing.Size(284, 32);
             this.SalasanaTB.TabIndex = 4;
             // 
-            // KayttajatunnusTB
+            // KayttajaTB
             // 
-            this.KayttajatunnusTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.KayttajatunnusTB.Location = new System.Drawing.Point(170, 53);
-            this.KayttajatunnusTB.Multiline = true;
-            this.KayttajatunnusTB.Name = "KayttajatunnusTB";
-            this.KayttajatunnusTB.Size = new System.Drawing.Size(284, 31);
-            this.KayttajatunnusTB.TabIndex = 3;
+            this.KayttajaTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.KayttajaTB.Location = new System.Drawing.Point(170, 53);
+            this.KayttajaTB.Multiline = true;
+            this.KayttajaTB.Name = "KayttajaTB";
+            this.KayttajaTB.Size = new System.Drawing.Size(284, 31);
+            this.KayttajaTB.TabIndex = 3;
             // 
-            // KirjauduBT
+            // KirjauduPainike
             // 
-            this.KirjauduBT.BackColor = System.Drawing.Color.Tomato;
-            this.KirjauduBT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.KirjauduBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.KirjauduBT.Location = new System.Drawing.Point(41, 179);
-            this.KirjauduBT.Name = "KirjauduBT";
-            this.KirjauduBT.Size = new System.Drawing.Size(413, 53);
-            this.KirjauduBT.TabIndex = 2;
-            this.KirjauduBT.Text = "Kirjaudu";
-            this.KirjauduBT.UseVisualStyleBackColor = false;
+            this.KirjauduPainike.BackColor = System.Drawing.Color.Tomato;
+            this.KirjauduPainike.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.KirjauduPainike.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KirjauduPainike.ForeColor = System.Drawing.Color.White;
+            this.KirjauduPainike.Location = new System.Drawing.Point(41, 179);
+            this.KirjauduPainike.Name = "KirjauduPainike";
+            this.KirjauduPainike.Size = new System.Drawing.Size(413, 53);
+            this.KirjauduPainike.TabIndex = 2;
+            this.KirjauduPainike.Text = "Kirjaudu";
+            this.KirjauduPainike.UseVisualStyleBackColor = false;
+            this.KirjauduPainike.Click += new System.EventHandler(this.KirjauduPainike_Click);
             // 
             // label3
             // 
@@ -126,20 +130,30 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Käyttäjä: ";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.KirjauduLB);
+            this.panel1.Location = new System.Drawing.Point(12, 152);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(497, 66);
+            this.panel1.TabIndex = 3;
+            // 
             // Kirjautumisikkuna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(527, 516);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Kirjautumisikkuna";
             this.Text = "Kirjautumisikkuna";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -147,13 +161,14 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label KirjauduLB;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox SalasanaTB;
-        private System.Windows.Forms.TextBox KayttajatunnusTB;
-        private System.Windows.Forms.Button KirjauduBT;
+        private System.Windows.Forms.TextBox KayttajaTB;
+        private System.Windows.Forms.Button KirjauduPainike;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Kirjautumisikkuna/Kirjautumisikkuna/Kirjautumisikkuna.cs
+++ Kirjautumisikkuna/Kirjautumisikkuna/Kirjautumisikkuna.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient; 
+
 
 namespace Kirjautumisikkuna
 {
@@ -16,5 +18,51 @@ namespace Kirjautumisikkuna
         {
             InitializeComponent();
         }
+
+        private void KirjauduPainike_Click(object sender, EventArgs e)
+        {
+            Yhdista tietokantaan = new Yhdista(); 
+            DataTable taulu = new DataTable();
+            MySqlDataAdapter adapteri = new MySqlDataAdapter; 
+            MySqlCommand komento  = new MySqlCommand();
+
+            String kysely = "SELECT kayttajanimi, salasana FROM asiakkaat WHERE kayttajanimi = @kn AND salasana = @ss"; 
+
+            komento.CommandText = kysely;
+            komento.Connection = tietokantaan.OtaYhteys();
+
+            komento.Parameters.Add("@kn", MySqlDbType.VarChar).Value = KayttajaTB.Text;
+            komento.Parameters.Add("@ss", MySqlDbType.VarChar).Value = SalasanaTB.Text;
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu); 
+
+            if(taulu.Rows.Count > 0)
+            {
+                this.Hide();
+                Paaikkuna plomake = new Paaikkuna();
+                plomake.Show(); 
+            }
+
+            else
+            {
+                if (KayttajaTB.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Syötä käyttäjätunnus kirjautuaksesi", "Käyttäjäntunnus on tyhjä", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                }
+                else if (SalasanaTB.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Syötä salasana kirjautuaksesi", "Salasana on tyhjä", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                else
+                {
+                    MessageBox.Show("Käyttäjätunnusta tai salasanaa ei löydy", "Tietoja ei löydy", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                    
+            }
+        }
+
+
     }
 }
